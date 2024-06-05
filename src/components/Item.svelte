04c864c3aad739201361c47da8a05829bba526dc
Price Tula <pricetula@gmail.com>
2024-06-05 16:59:49 +0300

    items

--- src/components/Item.svelte
+++ src/components/Item.svelte
@@ -3,14 +3,18 @@
         draggable,
         dropTargetForElements,
     } from "@atlaskit/pragmatic-drag-and-drop/element/adapter";
-    import { combine } from '@atlaskit/pragmatic-drag-and-drop/combine';
+    import { combine } from "@atlaskit/pragmatic-drag-and-drop/combine";
 
-	export let item = null;
-    export let index = null;
-    export let level = null;
+    export let item: {
+        id: number;
+        text: string;
+        children?: any[];
+    };
+    export let index: number;
+    export let level: number;
 
     let toggleOpen = false;
-    let buttonRef = null;
+    let buttonRef: any = null;
     const indentPerLevel = 32;
 
     $: if (buttonRef) {
@@ -21,7 +25,7 @@
                 //     console.log("draggable:onDragStart", e);
                 // },
                 onDrop(e) {
-                    console.log("draggable:onDrop", e);
+                    // console.log("draggable:onDrop", e);
                 },
             }),
             dropTargetForElements({
@@ -30,15 +34,28 @@
                 //     console.log("dropTargetForElements:onDragStart", e);
                 // },
                 onDrop(e) {
-                    console.log("dropTargetForElements:onDrop", e);
+                    // console.log("dropTargetForElements:onDrop", e);
                 },
-            })
-        )
+                // getData: ({ input, element }) => {
+                //     // your base data you want to attach to the drop target
+                //     const data = {
+                //     itemId: 'A',
+                //     };
+                //     // this will 'attach' the closest edge to your `data` object
+                //     return attachClosestEdge(data, {
+                //         input,
+                //         element,
+                //         // you can specify what edges you want to allow the user to be closest to
+                //         allowedEdges: ['top', 'bottom'],
+                //     });
+                // },
+            }),
+        );
     }
 </script>
 
 <button
-    id={item.id}
+    id={item.id + ''}
     data-testid={item.id}
     data-index={index}
     data-level={level}
@@ -50,10 +67,6 @@
         {item.text}
     </div>
     {#each item.children || [] as child, index}
-        <svelte:self
-            {child}
-            {index}
-            level={level + 1}
-        />
+        <svelte:self item={child} {index} level={level + 1} />
     {/each}
 </button>
--- src/routes/dnd2/+page.svelte
+++ src/routes/dnd2/+page.svelte
@@ -1,32 +1,109 @@
 <script lang="ts">
-	import { monitorForElements } from '@atlaskit/pragmatic-drag-and-drop/element/adapter';
-	import Item from '../components/Item.svelte';
+	import {
+		monitorForElements,
+		dropTargetForElements,
+	} from "@atlaskit/pragmatic-drag-and-drop/element/adapter";
+	import { combine } from "@atlaskit/pragmatic-drag-and-drop/combine";
+	import Item from "../components/Item.svelte";
+	import { text } from "@sveltejs/kit";
 
-	let ref = null;
-	const items = Array.from({ length: 10 }, (_, i) => ({
-		id: i,
-		text: `Item ${i}`,
-
-	}));
-
+	let ref: any = null;
+	let itemsFromDb = [
+		{
+			id: '1',
+			text: "Item 1",
+			parentId: null,
+		},
+		{
+			id: '2',
+			text: "Item 2",
+			parentId: null,
+		},
+		{
+			id: '3',
+			text: "Item 3",
+			parentId: null,
+		},
+		{
+			id: '4',
+			text: "Item 4",
+			parentId: '1',
+		},
+		{
+			id: '5',
+			text: "Item 5",
+			parentId: '1',
+		},
+		{
+			id: '6',
+			text: "Item 6",
+			parentId: '4',
+		},
+	];
+	$: console.log("itemsFromDb", itemsFromDb);
+	$: items = convertToTree(itemsFromDb);
 	$: if (ref) {
-		monitorForElements({
-			canMonitor: ({ source }) => true,
-			onDrop: console.log,
+		combine(
+			dropTargetForElements({
+				element: ref,
+			}),
+			monitorForElements({
+				canMonitor: ({ source }) => true,
+				onDrop: handleDrop,
+			}),
+		);
+	}
+	function convertToTree(data) {
+		const map = {};
+		for (const item of data) {
+			map[item.id] = item;
+			item.children = [];
+		}
+
+		for (const item of data) {
+			if (item.parentId !== null) {
+				const parent = map[item.parentId];
+				if (parent) {
+					parent.children.push(item);
+				}
+			}
+		}
+
+		const roots = [];
+		for (const item of data) {
+			if (item.parentId === null) {
+				roots.push(item);
+			}
+		}
+
+		return roots;
+	}
+	function handleDrop(e: any) {
+		if (
+			!e ||
+			!e?.source?.element ||
+			!e?.location?.current?.dropTargets?.[0]?.element
+		)
+			return;
+		const elementBeingDropped = e.source.element;
+		const dropTarget = e.location.current.dropTargets[0].element;
+
+		itemsFromDb = itemsFromDb.map((item) => {
+			if (item.id === elementBeingDropped?.dataset?.testid) {
+				return {
+					...item,
+					parentId: dropTarget?.dataset?.testid || null,
+				};
+			}
+			return item;
 		});
 	}
 </script>
 
-<div bind:this={ref} id="dnd-test">
+<div bind:this={ref} id="dnd-test" style="background-color: blue;">
 	{#each items as item, index}
-		<Item
-			{index}
-			{item}
-			level={0}
-		/>
+		<Item {index} {item} level={0} />
 	{/each}
 </div>
 
-<div>
-	SOME FAKE BOX
-</div>
+<div>SOME FAKE BOX</div>
